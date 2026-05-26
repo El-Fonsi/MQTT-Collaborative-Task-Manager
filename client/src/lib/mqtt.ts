96b4e6c8ff89110f93cd25544514df70b47cf984
FonsE143 <cortavistafonci@gmail.com>
2026-05-26 21:02:24 +0800

    SANA MQTT NA

--- client/src/lib/mqtt.ts
+++ client/src/lib/mqtt.ts
@@ -1,6 +1,6 @@
 import mqtt from 'mqtt';
 
-const MQTT_URL = import.meta.env.VITE_MQTT_URL || 'wss://aa7ddfb185e046a88e51c04a1b154971.s1.eu.hivemq.cloud:8884';
+const MQTT_URL = import.meta.env.VITE_MQTT_URL || 'wss://aa7ddfb185e046a88e51c04a1b154971.s1.eu.hivemq.cloud:8884/mqtt';
 const MQTT_USERNAME = import.meta.env.VITE_MQTT_USERNAME || 'Alfonse.203';
 const MQTT_PASSWORD = import.meta.env.VITE_MQTT_PASSWORD || 'Lacambra2003';
 
